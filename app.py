b548ce235ba7548af67bdc2415ce64406777e9a5
Ayush <patel.ayush08@gmail.com>
2018-04-03 17:36:50 +0530

    Commenting

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+# imports
 from flask import Flask, render_template, logging, redirect, url_for
 # from flask_pymongo import PyMongo
 from pymongo import MongoClient
 
+# Initializing MongoDB
 client = MongoClient('localhost', 27017)
 db = client.iitdost
 
+# Collections
 staffs = db.staffs
 students = db.students
 appointments = db.appointments
 
+# Initializing app
 app = Flask(__name__)
 # app.config["MONGO_DBNAME"]='iitdost'
 # mongo = PyMongo(app)
 
+
+# Homepage
 @app.route('/')
 def index():
 	# online_users = mongo.db.users.find({'online': 'True'})
@@ -20,12 +26,15 @@ def index():
 	return render_template('index.html', online_users=online_users)
 	# app.logger.info(app.name)
 
-@app.route('/add/<string:name>/<string:password>/')
-def add_user(username,password):
+# Adding staffs
+@app.route('/add_staff/<string:name>/<string:password>/')
+def add_staff(username,password):
 	staff = {'username':username, 'password':password, 'online':True}
 	staffs.insert(staff)
 	return redirect(url_for('index'))
 
+
+# Remove all the data
 @app.route('/refresh')
 def refresh():
 	# db.users.drop()
@@ -35,5 +44,6 @@ def refresh():
 	return redirect(url_for('index'))
 
 
+# Starting App
 if __name__ == '__main__':
 	app.run(debug=True)
