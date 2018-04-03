b958a8339a64516fd288cf1b2704c01cb2f0f6e4
Ayush <patel.ayush08@gmail.com>
2018-04-03 10:39:39 +0530

    Made index route and template

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run()
