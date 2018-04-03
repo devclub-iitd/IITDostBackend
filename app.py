24f44b3180c03aa36286946d92e001b19fe52a32
Ayush <patel.ayush08@gmail.com>
2018-04-03 10:57:27 +0530

    Instantized DB

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, logging
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -6,7 +6,9 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	online_users = mongo.db.users.find({'online': True})
+	return render_template('index.html', online_users=online_users)
+	# app.logger.info(app.name)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
