1309e133df083b5d969cf6aa18239acd0dba2234
Ayush <patel.ayush08@gmail.com>
2018-04-03 16:58:53 +0530

    Working Flask-PyMongo

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, logging
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"]='iitdost'
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-	online_users = mongo.db.users.find({'online': True})
+	online_users = mongo.db.users.find({'online': 'True'})
 	return render_template('index.html', online_users=online_users)
 	# app.logger.info(app.name)
 
